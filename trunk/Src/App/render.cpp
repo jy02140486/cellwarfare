3e56bd25318e1f7ceb335625287b3a69f243eb9a
jy02140486@gmail.com <jy02140486@gmail.com@7e182df5-b8f1-272d-db4e-b61a9d634eb1>
2011-03-17 10:14:19 +0000

    more framework stuff

--- trunk/Src/App/render.cpp
+++ trunk/Src/App/render.cpp
@@ -5,10 +5,6 @@ void T_App::render()
 	mpDisplayWindow->get_gc().clear(CL_Colorf::black);
 
 	mpWinManager->draw_windows(mpDisplayWindow->get_gc());
-
-	tcell->draw(&mpDisplayWindow->get_gc(),tcell->self->GetPosition().x,tcell->self->GetPosition().y);
-	
-//	tlv->lvdes->draw_text(mpDisplayWindow->get_gc(),80,80,"stage11111111111111111111",CL_Colorf::yellow);
 		
 	mpDisplayWindow->flip();
 }
