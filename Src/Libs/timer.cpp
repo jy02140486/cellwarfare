6f4168efde8e25600e242380cec61dafffdc12b0
jy02140486@gmail.com <jy02140486@gmail.com@7e182df5-b8f1-272d-db4e-b61a9d634eb1>
2011-03-23 04:24:33 +0000

    working on level switching

--- Src/Libs/timer.cpp
+++ Src/Libs/timer.cpp
@@ -1,5 +1,6 @@
 #include "timer.h"
 
+
 void Timer::init(int seconds,bool repeatflag)
 {
 	length=seconds;
@@ -8,7 +9,14 @@ void Timer::init(int seconds,bool repeatflag)
 
 int Timer::get_curSec()
 {
-	return (CL_System::get_time()-startflag)/1000;
+	int cur=(CL_System::get_time()-startflag)/1000;
+	int res = cur>length?-1:cur;
+	if (res==-1)
+	{
+		CL_Timer::stop();
+		return -1;
+	}
+	else return cur;
 }
 
 void Timer::begin()
@@ -20,4 +28,11 @@ void Timer::begin()
 Timer::Timer()
 {
 
+}
+
+CL_Callback_v0 & Timer::func_expired()
+{
+	CL_Console::write_line("aaa");
+	CL_Callback_v0 callback;
+	return callback;
 }
