cfa3e68867e31333f20a68a14d1a1bb21654f695
jy02140486@gmail.com <jy02140486@gmail.com@7e182df5-b8f1-272d-db4e-b61a9d634eb1>
2011-05-19 03:43:59 +0000

    working on intruder generation of bf initialization.

--- Src/Libs/Conveter.cpp
+++ Src/Libs/Conveter.cpp
@@ -5,7 +5,7 @@ CL_Vec2i Conveter::Vec2from_b_to_c(b2Vec2 ref)
 	return CL_Vec2i(ref.x,ref.y);
 }
 
-b2Vec2* Conveter::Vec2from_c_to_b(CL_Vec2i ref)
+b2Vec2 Conveter::Vec2from_c_to_b(CL_Vec2i ref)
 {
-	return new b2Vec2(ref.x,ref.y);
+	return b2Vec2(ref.x,ref.y);
 }
