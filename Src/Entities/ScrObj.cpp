b04724bcaa9c84c8df70498271af3ec2e2857bbc
jy02140486@gmail.com <jy02140486@gmail.com@7e182df5-b8f1-272d-db4e-b61a9d634eb1>
2011-05-24 14:21:23 +0000

    ScrObj structure improvement. cancel two exceed objstates.

--- Src/Entities/ScrObj.cpp
+++ Src/Entities/ScrObj.cpp
@@ -15,12 +15,6 @@ void ScrObj::draw(CL_GraphicContext *gc)
 	CL_Draw::circle(*gc,pos->x,pos->y,refradius,CL_Colorf(255.0f,0.0f,0.0f,0.4f));
 	switch(ObjState)
 	{
-	case MOUSEON:
-		CL_Draw::circle(*gc,pos->x,pos->y,refradius/2,CL_Colorf(0.0f,0.0f,255.0f,0.6f));
-		break;
-	case SELECTED:
-		CL_Draw::circle(*gc,pos->x,pos->y,refradius/2,CL_Colorf(0.0f,255.0f,0.0f,0.6f));
-		break;
 	case CANNON:
 		CL_Draw::circle(*gc,pos->x,pos->y,refradius,CL_Colorf::gray);
 		break;
@@ -29,6 +23,10 @@ void ScrObj::draw(CL_GraphicContext *gc)
 		break;
 	case LYMPH:
 		CL_Draw::circle(*gc,pos->x,pos->y,refradius,CL_Colorf::yellow);
+	}
+	if (isCousurOn(posMouse))
+	{
+		CL_Draw::circle(*gc,pos->x,pos->y,refradius*0.4,CL_Colorf::green);
 	}
 }
 
@@ -40,16 +38,7 @@ bool ScrObj::isCousurOn(CL_Point mouse)
 	}else return false;
 }
 
-void ScrObj::stateSwitching()
-{
-	if (isCousurOn(posMouse)&&ObjState!=SELECTED)
-	{
-		ObjState=MOUSEON;
-	}else if(ObjState!=SELECTED)
-		ObjState=NORMAL;
-	if(datas->intruder>0)
-		ObjState=INTRUDED;
-}
+
 
 void ScrObj::update(CL_Point curMouse)
 {
